94929562b3b2942124308d5b3bbb0a00e3e876cc
vitalii <vitalii.cherukha@gmail.com>
2025-08-08 17:04:07 +0300

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import css from './App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo'
-import VoteOptions from '../VoteOption/VoteOptions'
+import VoteOptions from '../VoteOptions/VoteOptions'
 import VoteStats from '../VoteStats/VoteStats'
 import Notification from '../Notification/Notification'
 import { type VoteType, type Votes } from '../../types/votes'
